f0ed3ae2b1bc310131084e4be176d93af8e0af59
kizuyoko <kizuyoko@gmail.com>
2023-11-15 10:33:54 +0100

    task 6, 7 - Output a list of dummy events to the EventsPage, Output the ID of the selected event

--- adv-routing/frontend/src/pages/EventDetail.js
+++ adv-routing/frontend/src/pages/EventDetail.js
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
 
 function EventDetailPage() {
+  const params = useParams();
   return (
     <>
-      <h1>Event Detail Page</h1>
+      <h1>Event Detail Page, {params.eventId}</h1>
     </>
   );
 } 
