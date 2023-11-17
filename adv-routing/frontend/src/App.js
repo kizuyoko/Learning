2896697185d7f57220c8cdc3d4c2c782422847e8
kizuyoko <kizuyoko@gmail.com>
2023-11-17 10:16:04 +0100

    477. Reusing Actions via Request Methods

--- adv-routing/frontend/src/App.js
+++ adv-routing/frontend/src/App.js
@@ -5,11 +5,12 @@ import EventDetailPage, {
   loader as eventsDetailLoader, 
   action as deleteEventAction
 } from "./pages/EventDetail";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
+import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
+import { action as manipulateEventAction } from './components/EventForm';
 
 const router = createBrowserRouter([
   {
@@ -39,13 +40,17 @@ const router = createBrowserRouter([
                 element: <EventDetailPage />,
                 action: deleteEventAction
               },
-              { path: 'edit', element: <EditEventPage />},
+              { 
+                path: 'edit', 
+                element: <EditEventPage />,
+                action: manipulateEventAction
+              },
             ],
           }, 
           { 
             path: 'new', 
             element: <NewEventPage />,
-            action: newEventAction
+            action: manipulateEventAction
           }
         ]
       }
