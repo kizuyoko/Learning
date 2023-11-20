9cf762d273cd4c5d00c42aee89a11ed88fdeaacb
kizuyoko <kizuyoko@gmail.com>
2023-11-20 15:21:44 +0100

    496. Adding Route Protection

--- authentication/frontend/src/util/auth.js
+++ authentication/frontend/src/util/auth.js
@@ -6,3 +6,13 @@ export function getAuthToken() {
 export function tokenLoader() {
   return getAuthToken();
 }
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect('/auth');
+  }
+
+  return null;
+}
