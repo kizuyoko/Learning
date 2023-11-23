55f7e5ecfabfb8f8a08dba0ecd1c57c36e726d14
kizuyoko <kizuyoko@gmail.com>
2023-11-23 11:48:51 +0100

    537. Extracting Dynamic Parameter Values

--- nextjs-meetup/pages/news/[newsId].js
+++ nextjs-meetup/pages/news/[newsId].js
@@ -1,4 +1,14 @@
+import { useRouter } from "next/router";
+
 function DetailPage() {
+  const router = useRouter();
+
+  //console.log(router.query.newsId);
+  const newsId = router.query.newsId;
+
+  // Send a request to the backend API 
+  // to fetch the news item with newsId
+
   return <h1>Detail Page</h1>
 }
 
