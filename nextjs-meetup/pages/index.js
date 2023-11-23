fb950994570e49c677d157f53fdedebea6a957ac
kizuyoko <kizuyoko@gmail.com>
2023-11-23 13:15:49 +0100

    538. Linking Between Pages

--- nextjs-meetup/pages/index.js
+++ nextjs-meetup/pages/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+import { Fragment } from "react";
 function HomePage() {
-  return <h1>Home Page</h1>
+  return <Fragment>
+  <h1>Home Page</h1>
+  <ul>
+    {/* Not reccomended to use a-tag because it reloads. Instead, use Link. */}
+    {/* <li><a href='/news'>News</a></li> */}
+    <li><Link href='/news'>News</Link></li>
+  </ul>
+</Fragment>
 }
 
 export default HomePage;
