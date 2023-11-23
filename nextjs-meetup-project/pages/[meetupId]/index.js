6c754a1b2395161572719ad92b2b43d92e6c25a2
kizuyoko <kizuyoko@gmail.com>
2023-11-23 15:09:10 +0100

    545. Adding Custom Components & CSS Modules

---
+++ nextjs-meetup-project/pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail 
+      image='https://cdn-az.allevents.in/events5/banners/25c07fc896354de038d8168acad971c381788a29df07d9820ebb731677a9a216-rimg-w1200-h856-gmir.jpg?v=1699883486'
+      title='First'
+      address='hhhhh'
+      description='ddddd'
+    />
+  );
+}
