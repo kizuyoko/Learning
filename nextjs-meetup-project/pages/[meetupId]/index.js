cacff3f52ef81af99bba21099c2bb02b45ed0e85
kizuyoko <kizuyoko@gmail.com>
2023-11-24 10:36:09 +0100

    550. Working with Params for SSG Data Fetching

--- nextjs-meetup-project/pages/[meetupId]/index.js
+++ nextjs-meetup-project/pages/[meetupId]/index.js
@@ -4,9 +4,43 @@ export default function MeetupDetails() {
   return (
     <MeetupDetail 
       image='https://cdn-az.allevents.in/events5/banners/25c07fc896354de038d8168acad971c381788a29df07d9820ebb731677a9a216-rimg-w1200-h856-gmir.jpg?v=1699883486'
-      title='First'
+      title='MeetupDetail'
       address='hhhhh'
       description='ddddd'
     />
   );
+}
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      { params: {
+        meetupId: 'm1',  
+      }},
+      { params: {
+        meetupId: 'm2',  
+      }},
+    ]
+  }
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  // This logs on the terminal, not on the browser.
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image: 'https://cdn-az.allevents.in/events5/banners/25c07fc896354de038d8168acad971c381788a29df07d9820ebb731677a9a216-rimg-w1200-h856-gmir.jpg?v=1699883486',
+        id: 'm1',
+        title: 'MeetupData',
+        address: 'hhhhh',
+        description: 'ddddd',
+      }
+    }
+  }
 }
--- nextjs-meetup-project/pages/index.js
+++ nextjs-meetup-project/pages/index.js
@@ -14,6 +14,13 @@ const DUMMY_MEETUPS = [
     image: 'https://cdn-az.allevents.in/events5/banners/25c07fc896354de038d8168acad971c381788a29df07d9820ebb731677a9a216-rimg-w1200-h856-gmir.jpg?v=1699883486',
     address: 'Adress 1, Street 3',
     description: 'The second meetup'
+  },
+  {
+    id: 'm3',
+    title: 'Third meetup',
+    image: 'https://cdn-az.allevents.in/events5/banners/25c07fc896354de038d8168acad971c381788a29df07d9820ebb731677a9a216-rimg-w1200-h856-gmir.jpg?v=1699883486',
+    address: 'Adress 1, Street 3',
+    description: 'The thrid meetup'
   }
 ]
 
