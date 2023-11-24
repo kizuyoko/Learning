75fe6e22cb04a7df6d00fd3a47fd2f356cd35e5e
kizuyoko <kizuyoko@gmail.com>
2023-11-24 13:40:31 +0100

    555. Getting Data From The Database

--- nextjs-meetup-project/pages/index.js
+++ nextjs-meetup-project/pages/index.js
@@ -1,28 +1,6 @@
-import MeetupList from "../components/meetups/MeetupList"
+import { MongoClient } from "mongodb";
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First meetup',
-    image: 'https://memberpress.com/wp-content/uploads/2019/10/Member-Meetup@2x.png',
-    address: 'Adress 1, Street 3',
-    description: 'The first meetup'
-  },
-  {
-    id: 'm2',
-    title: 'A Second meetup',
-    image: 'https://cdn-az.allevents.in/events5/banners/25c07fc896354de038d8168acad971c381788a29df07d9820ebb731677a9a216-rimg-w1200-h856-gmir.jpg?v=1699883486',
-    address: 'Adress 1, Street 3',
-    description: 'The second meetup'
-  },
-  {
-    id: 'm3',
-    title: 'Third meetup',
-    image: 'https://cdn-az.allevents.in/events5/banners/25c07fc896354de038d8168acad971c381788a29df07d9820ebb731677a9a216-rimg-w1200-h856-gmir.jpg?v=1699883486',
-    address: 'Adress 1, Street 3',
-    description: 'The thrid meetup'
-  }
-]
+import MeetupList from "../components/meetups/MeetupList"
 
 export default function HomePage(props) {
   return (
@@ -47,11 +25,27 @@ export default function HomePage(props) {
 // getStaticProps is a Next.js feature. If you export a function called getStaticProps (Static Site Generation) from a page, Next.js will pre-render this page at build time using the props returned by getStaticProps. There is an another way to do above.
 export async function getStaticProps() {
   // fetch data from API
+  const client = await MongoClient.connect('mongodb+srv://kizuyoko:packardbell1@cluster0.sgilcus.mongodb.net/meetups?retryWrites=true&w=majority'); 
+  
+  const db = client.db(); 
+    
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map(meetup => ({
+        id: meetup._id.toString(),
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        description:meetup.description 
+      }))
     },
-    revalidate: 10
+    revalidate: 1
   };
 }
 
