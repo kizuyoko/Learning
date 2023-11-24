8e851422b3a02e1a78ecb48a3aaa32a32e33a137
kizuyoko <kizuyoko@gmail.com>
2023-11-24 11:23:33 +0100

    553. Workingwith Mongo DB

--- nextjs-meetup-project/pages/api/new-meetup.js
+++ nextjs-meetup-project/pages/api/new-meetup.js
@@ -1,11 +1,21 @@
+import { MongoClient } from 'mongodb'; 
+
 // /api/new-meetup
 // Only POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect('mongodb+srv://kizuyoko:packardbell1@cluster0.sgilcus.mongodb.net/?retryWrites=true&w=majority'); 
+    const db = client.db(); 
+    
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({message: 'Meetup inserted!'});
   }
 }
 
