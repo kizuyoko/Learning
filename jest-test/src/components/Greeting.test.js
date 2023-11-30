061111b85f07a0aab80e38af081b2a6b8ab60ba0
kizuyoko <kizuyoko@gmail.com>
2023-11-30 11:14:56 +0100

    590. Grouping Tests Together With Test Suites

--- jest-test/src/components/Greeting.test.js
+++ jest-test/src/components/Greeting.test.js
@@ -1,13 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from "./Greeting";
 
-test('renders Hello World as a test', () => {
-  // Arrange
-  render(<Greeting />);
+describe('Greetin component', () => {
+  test('renders Hello World as a test', () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act... Nothing
+    // Act... Nothing
 
-  // Assert
-  const helloWorldElement = screen.getByText('Hello World!', { exact: false });
-  expect(helloWorldElement).toBeInTheDocument();
+    // Assert
+    const helloWorldElement = screen.getByText('Hello World!', { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
+
