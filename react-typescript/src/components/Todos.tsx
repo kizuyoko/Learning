f04bd2c4f1a81e6f5fe2bbd2bcbfc6aad790ee92
kizuyoko <kizuyoko@gmail.com>
2023-12-04 10:32:37 +0100

    611. Time to Practice: Exercise Time!

--- react-typescript/src/components/Todos.tsx
+++ react-typescript/src/components/Todos.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import TodoItem from './TodoItem';
 import Todo from '../models/todo';
 
 // FC means Functional Component, generic type.
@@ -7,7 +7,7 @@ const Todos: React.FC<{items: Todo[]}> = (props) => {
   return (
     <ul>
       {props.items.map(item => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );  
