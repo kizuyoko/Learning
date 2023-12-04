780ed0060d7d3083468b9421d2d7ddd06c212796
kizuyoko <kizuyoko@gmail.com>
2023-12-04 16:17:50 +0100

    617. Time to Practice: Removing a Todo

--- react-typescript/src/components/TodoItem.tsx
+++ react-typescript/src/components/TodoItem.tsx
@@ -1,9 +1,17 @@
 import classes from './TodoItem.module.css';
 import React from 'react';
 
-const TodoItem:React.FC<{ text: string }>  = (props) => {
+const TodoItem: React.FC<{ 
+  text: string; 
+  onRemoveTodo: () => void 
+}>  = (props) => {
   return (
-    <li className={classes.item}>{props.text}</li>
+    <li 
+      className={classes.item}
+      onClick={props.onRemoveTodo}
+    >
+      {props.text}
+    </li>
   );
 };
 
--- react-typescript/src/components/Todos.tsx
+++ react-typescript/src/components/Todos.tsx
@@ -4,11 +4,18 @@ import Todo from '../models/todo';
 import classes from './Todos.module.css';
 
 // FC means Functional Component, generic type.
-const Todos: React.FC<{items: Todo[]}> = (props) => {
+const Todos: React.FC<{
+  items: Todo[]; 
+  onRemoveTodo: (id: string) => void 
+}> = (props) => {
   return (
     <ul className={classes.todos}>
       {props.items.map(item => (
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem 
+          key={item.id} 
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );  
