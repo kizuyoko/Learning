e0233b058fee3a4419470dc7bc550d53ef48ec6f
kizuyoko <kizuyoko@gmail.com>
2023-12-07 14:45:21 +0100

    651. Refactoring Route Components & More Nesting

--- react-core/src/components/PostsList.jsx
+++ react-core/src/components/PostsList.jsx
@@ -1,16 +1,8 @@
 import classes from './PostsList.module.css';
-import NewPost from './NewPost';
 import Post from './Post';
-import Modal from './Modal';
 import { useState, useEffect } from 'react';
 
-function PostsList({ isPosting, onStopPosting }) {
-  //Doable but not good because of infinitive loop. 
-  //Better to use 'useEffect' instead
-  // fetch('http://localhost:8080/posts')
-  //   .then(response => response.json())
-  //   .then(data => setPosts(data.posts));
-
+function PostsList() {
   const [posts, setPosts] = useState([]);
   const [isFetching, setIsFetching] = useState(false); 
 
@@ -42,14 +34,6 @@ function PostsList({ isPosting, onStopPosting }) {
 
   return(
     <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost 
-            onCancel={onStopPosting}
-            onAddPost={addPostHandler}
-          />
-        </Modal>
-      )}
       {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map(post => 
--- react-core/src/routes/RootLayout.jsx
+++ react-core/src/routes/RootLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 import MainHeader from "../components/MainHeader";
 
 function RootLayout() {
