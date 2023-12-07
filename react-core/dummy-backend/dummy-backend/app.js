098d4202d50b55c1ab759ad9cc93c1a78883d89b
kizuyoko <kizuyoko@gmail.com>
2023-12-07 13:47:26 +0100

    647. Handle Loading State

--- react-core/dummy-backend/dummy-backend/app.js
+++ react-core/dummy-backend/dummy-backend/app.js
@@ -18,7 +18,9 @@ app.use((req, res, next) => {
 
 app.get('/posts', async (req, res) => {
   const storedPosts = await getStoredPosts();
-  // await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
+
+  // This code is to make the loading time slow to learn about Promise
+  await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
   res.json({ posts: storedPosts });
 });
 
--- react-core/src/components/PostsList.jsx
+++ react-core/src/components/PostsList.jsx
@@ -12,18 +12,21 @@ function PostsList({ isPosting, onStopPosting }) {
   //   .then(data => setPosts(data.posts));
 
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false); 
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch('http://localhost:8080/posts');
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
-
     fetchPosts();
   }, []);
 
   const addPostHandler = (postData) => {
+    setIsFetching(true);
     fetch('http://localhost:8080/posts', {
       method: 'POST',
       body: JSON.stringify(postData),
@@ -34,6 +37,7 @@ function PostsList({ isPosting, onStopPosting }) {
 
     setPosts((existingPosts) => [postData, ...existingPosts]);
     //console.log(posts)
+    setIsFetching(false);
   };
 
   return(
@@ -46,7 +50,7 @@ function PostsList({ isPosting, onStopPosting }) {
           />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map(post => 
             <Post
@@ -57,12 +61,14 @@ function PostsList({ isPosting, onStopPosting }) {
           )}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white'}}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
         </div>
       )}
+      {isFetching && (
+      <div style={{color: 'white'}}><p>Loading data...</p></div>)}
     </>  
   );
 }
