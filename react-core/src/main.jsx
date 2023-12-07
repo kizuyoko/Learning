12e05417ce061411841ee4bee7ec51caad26e978
kizuyoko <kizuyoko@gmail.com>
2023-12-07 14:07:52 +0100

    649. Adding Routes

--- react-core/src/main.jsx
+++ react-core/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 import App from './App'
 import './index.css'
+import NewPost from './components/NewPost'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+
+  // This shows only <dialog>
+  { path: '/create-post', element: <NewPost /> }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
