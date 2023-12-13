0ff9d9e1d5d9021deb6a3d8f0d9ecaf12cfc3177
kizuyoko <kizuyoko@gmail.com>
2023-12-13 14:15:54 +0100

    Stories with traffic lights

--- storybook/src/stories/Light.stories.tsx
+++ storybook/src/stories/Light.stories.tsx
@@ -14,6 +14,38 @@ type Story = StoryObj<typeof meta>;
 
 export const Base: Story = {
   args: {
-
+    variant: 'green'
   }
+};
+
+export const Yellow: Story = {
+  args: {
+    variant: 'yellow'
+  }
+};
+
+export const Red: Story = {
+  args: {
+    variant: 'red'
+  }
+};
+
+export const Grouped: Story = {
+  render: (args) => (
+    <div
+      style={{
+        background: 'gray',
+        display:'flex',
+        flexDirection: 'column',
+        gap: 10,
+        border: '1px solid black',
+        width: 'max-content',
+        padding: 10
+      }}
+    >
+      <Light variant='red' />
+      <Light variant='yellow' />
+      <Light variant='green' />
+    </div>
+  ),
 };
--- storybook/src/stories/Light.tsx
+++ storybook/src/stories/Light.tsx
@@ -1,7 +1,19 @@
-type Props = {};
+type Props = {
+  variant: 'green' | 'yellow' | 'red'
+};
 
-const Light = (props: Props) => {
-  return <div>Light</div>;
+const Light = ({variant = 'green'}: Props) => {
+  return (
+    <div 
+      style={{
+        padding:20,
+        background: variant,
+        borderRadius: '50%',
+        width: 50,
+        height: 50
+      }}
+    ></div>
+  )
 };
 
 export default Light;
