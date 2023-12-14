8b9789696d5fc3364bf670833cf49df0ea788c08
kizuyoko <kizuyoko@gmail.com>
2023-12-14 15:39:53 +0100

    32. Testing Accessibility (a11y)

--- react-components-app-master/src/components/Greeting/Greeting.jsx
+++ react-components-app-master/src/components/Greeting/Greeting.jsx
@@ -14,6 +14,10 @@ export default function Greeting({ fontFamily }) {
       <h1>Hi{name && <span>, </span>}{name}!</h1>
       {!name && <p>Please fyll in your name below.</p>}
       {name && <p>Nice to meet you!</p>}
+      <label 
+        htmlFor='yourName'
+        className="block text-gray-700 text-sm font-bold mt-2" 
+      >Your name: </label>
       <input type="text" id='yourName' name='yourName' onChange={onChange} />
     </div>
   );
