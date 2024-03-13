a787fce2a9cc2410046e15686217be12efb118ef
kizuyoko <kizuyoko@gmail.com>
2024-03-13 13:42:13 +0100

    Component props

---
+++ typescript/react-basic-goals/src/components/CourseGoal.tsx
@@ -0,0 +1,15 @@
+export default function CourseGoal({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
+  return <article>
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+    <button>Delete</button>
+  </article>;
+}
