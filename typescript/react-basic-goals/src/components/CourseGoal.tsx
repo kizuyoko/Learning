d121a2fe5358003131d4354fa822223ca93d1fcf
kizuyoko <kizuyoko@gmail.com>
2024-03-13 15:25:27 +0100

    Delete button

--- typescript/react-basic-goals/src/components/CourseGoal.tsx
+++ typescript/react-basic-goals/src/components/CourseGoal.tsx
@@ -10,15 +10,24 @@ import {
 //  children: ReactNode;
 //}
 
-type CourseGoalProps = PropsWithChildren<{title:string}>;
+type CourseGoalProps = PropsWithChildren<{
+  id: number;
+  title:string;
+  onDelete: (id: number) => void;
+}>;
 
-export default function CourseGoal({ title, children }: CourseGoalProps) {
+export default function CourseGoal({ 
+  id, 
+  title, 
+  children, 
+  onDelete 
+}: CourseGoalProps) {
   return <article>
     <div>
       <h2>{title}</h2>
       {children}
     </div>
-    <button>Delete</button>
+    <button onClick={() => onDelete(id)}>Delete</button>
   </article>;
 }
 
