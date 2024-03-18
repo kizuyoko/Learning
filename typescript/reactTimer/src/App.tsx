0a7fcece39cec699c9f1108104d5681ce08c6d24
kizuyoko <kizuyoko@gmail.com>
2024-03-18 15:19:39 +0100

    ComponentPropsWithoutRef

--- typescript/reactTimer/src/App.tsx
+++ typescript/reactTimer/src/App.tsx
@@ -1,5 +1,12 @@
+import Input from "./components/Input";
+
 function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <main>
+      <Input label="Your name?" id="name" type="text" disabled />
+      <Input label="Your age?" id="age" type="number" />
+    </main>
+  );
 }
 
 export default App;
