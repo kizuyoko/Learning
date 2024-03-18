b9da6385807fc87fed420975f0f581fb553240bf
kizuyoko <kizuyoko@gmail.com>
2024-03-18 15:32:59 +0100

    props: ButtonProps | AnchorProps

--- typescript/reactTimer/src/App.tsx
+++ typescript/reactTimer/src/App.tsx
@@ -1,3 +1,4 @@
+import Button from "./components/Button";
 import Input from "./components/Input";
 
 function App() {
@@ -5,6 +6,13 @@ function App() {
     <main>
       <Input label="Your name?" id="name" type="text" disabled />
       <Input label="Your age?" id="age" type="number" />
+
+      <p>
+        <Button el='button' className="button">A Button</Button>
+      </p>
+      <p>
+        <Button el='anchor' className="button" href='https://google.com'>A Link</Button>
+      </p>
     </main>
   );
 }
