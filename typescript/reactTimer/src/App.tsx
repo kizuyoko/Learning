78653ac8713ab3ad57230306eccf835d85fbba96
kizuyoko <kizuyoko@gmail.com>
2024-03-19 15:25:05 +0100

    Generics Component

--- typescript/reactTimer/src/App.tsx
+++ typescript/reactTimer/src/App.tsx
@@ -1,18 +1,14 @@
 import Button from "./components/Button";
-import Input from "./components/Input";
+//import Input from "./components/Input";
+import Container from "./components/Container";
 
 function App() {
   return (
     <main>
       {/* <Input label="Your name?" id="name" type="text" disabled />
       <Input label="Your age?" id="age" type="number" /> */}
-
-      <p>
-        <Button>A Button</Button>
-      </p>
-      <p>
+      <Container as={Button} onClick={() => {}}>Button</Container>
         <Button href='https://google.com'>A Link</Button>
-      </p>
     </main>
   );
 }
