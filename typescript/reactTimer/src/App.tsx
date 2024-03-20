1d6053498facb76beebbfa1f2dc8a5d482d9dc2e
kizuyoko <kizuyoko@gmail.com>
2024-03-20 15:24:05 +0100

    Moving to the next project with timer

--- typescript/reactTimer/src/App.tsx
+++ typescript/reactTimer/src/App.tsx
@@ -1,8 +1,11 @@
 import { useRef } from "react";
-import Button from "./components/Button";
-import Input from "./components/Input";
+//import Button from "./components/UI/Button";
+//import Input from "./components/UI/Input";
 //import Container from "./components/Container";
-import Form, { type FormHandle } from "./components/Form";
+import Form, { type FormHandle } from "./components/UI/Form";
+import AddTimer from "./components/AddTimer";
+import Header from "./components/Header";
+import Timers from "./components/Timers";
 
 function App() {
   // const input = useRef<HTMLInputElement>(null);
@@ -16,15 +19,20 @@ function App() {
   }
 
   return (
-    <main>
-      <Form onSave={handleSave} ref={customForm}>
-        <Input type='text' label='Name' id='name' />
-        <Input type='number' label='Age' id='age' />
-        <p>
-          <Button>Save</Button>
-        </p>
-      </Form>
-    </main>
+    <>
+      <Header />
+      <main>
+        {/* <Form onSave={handleSave} ref={customForm}>
+          <Input type='text' label='Name' id='name' />
+          <Input type='number' label='Age' id='age' />
+          <p>
+            <Button>Save</Button>
+          </p>
+        </Form> */}
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
   );
 }
 
