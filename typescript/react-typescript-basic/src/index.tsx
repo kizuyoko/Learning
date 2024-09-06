6d09537c4a09f0927c8c2e7bb4533b459e3bedd5
kizuyoko <kizuyoko@gmail.com>
2024-09-06 16:03:18 +0200

    Ref

--- typescript/react-typescript-basic/src/index.tsx
+++ typescript/react-typescript-basic/src/index.tsx
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom';
 import Parent from './props/Parent';
 import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+//import UserSearch from './state/UserSearch';
 import EventComponent from './events/EventComponent';
+import UserSearch from './refs/UserSearch';
 
 const App = () => {
   return (
