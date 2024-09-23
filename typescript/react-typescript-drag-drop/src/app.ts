ef78f12efef4dafade4823126a86d3f42861fdc7
kizuyoko <kizuyoko@gmail.com>
2024-09-23 15:17:39 +0200

    List Strat, Connect with DOM

--- typescript/react-typescript-drag-drop/src/app.ts
+++ typescript/react-typescript-drag-drop/src/app.ts
@@ -46,7 +46,7 @@ function validate(validatableInput: Validatable) {
   return isValid;
 }
 
-//autobind decorator
+// Autobind decorator; instead of adding .bind(this)
 function autobind(
   _: any,
   _2: string,
@@ -63,7 +63,35 @@ function autobind(
   return adjDescripter
 }
 
-//ProjectInoput Class
+//ProjectctList Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = 
+      this.type.toUpperCase() + ' PROJECTS';
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
+//ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -136,6 +164,8 @@ class ProjectInput {
     const userInput = this.gatherUserInput();
     if (Array.isArray(userInput)) {
       const [title, desc, people] = userInput;
+
+      // Done with fetching and validation!
       console.log(title, desc, people);
       this.clearInputs();
     }
@@ -150,4 +180,6 @@ class ProjectInput {
   }
 }
 
-const prjInput = new ProjectInput();
+const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList('active'); 
+const finishedProjectList = new ProjectList('finished'); 
