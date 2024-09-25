8783098b84eb9f591ba633ad3d4cc105da64b37b
kizuyoko <kizuyoko@gmail.com>
2024-09-25 13:08:52 +0200

    File and folders organizing with namespace

---
+++ typescript/react-typescript-drag-drop/src/decorators/autobind.ts
@@ -0,0 +1,18 @@
+namespace App {
+  // Autobind decorator; instead of adding .bind(this)
+  export function autobind(
+    _: any,
+    _2: string,
+    descripter: PropertyDescriptor
+  ) {
+    const originalMethod = descripter.value;
+    const adjDescripter: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      }
+    };
+    return adjDescripter
+  }
+}
