6bd9953690a70b8be4b14389518a8d33699dbe8d
kizuyoko <kizuyoko@gmail.com>
2024-09-25 13:21:16 +0200

    Organize more

--- typescript/react-typescript-drag-drop/src/app.ts
+++ typescript/react-typescript-drag-drop/src/app.ts
@@ -1,12 +1,7 @@
 // Import in TS starts with 3 slashes (///) and XML-tag reference, put the code in namespace.
 
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project-state.ts" />
-/// <reference path="util/validation.ts" />
-/// <reference path="decorators/autobind.ts" />
+
 /// <reference path="components/project-input.ts" />
-/// <reference path="components/project-item.ts" />
 /// <reference path="components/project-list.ts" />
 namespace App {
   new ProjectInput();
