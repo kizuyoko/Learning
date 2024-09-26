0cc5ff390a8e81d8a11f85d40d1295d8b0f91421
kizuyoko <kizuyoko@gmail.com>
2024-09-26 14:18:38 +0200

    start with env-file

--- typescript/typescript-place/src/app.ts
+++ typescript/typescript-place/src/app.ts
@@ -1,10 +1,13 @@
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+
+
 function searchAddressHandler(event: Event) {
   event. preventDefault();
   const enteredAddress = addressInput.value;
 
+  //To Google API!!!
 }
 
 form.addEventListener('submit', searchAddressHandler);
