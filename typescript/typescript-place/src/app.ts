c1745886426177e706f9533a56f9863203c96777
kizuyoko <kizuyoko@gmail.com>
2024-09-26 14:31:38 +0200

    .env

--- typescript/typescript-place/src/app.ts
+++ typescript/typescript-place/src/app.ts
@@ -1,3 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config(); // Load environment variables from .env file
+const googleApiKey = process.env.GOOGLE_API_KEY; // Access the GOOGLE_API_KEY
+
+// Use googleApiKey in your application
+console.log(googleApiKey); // Example usage
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
