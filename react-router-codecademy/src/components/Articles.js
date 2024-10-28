fc95ca5ce80e894fb5539ecb23c06169ca25c05a
kizuyoko <kizuyoko@gmail.com>
2024-10-28 13:31:33 +0100

    useNavigate, useSearchParams

--- react-router-codecademy/src/components/Articles.js
+++ react-router-codecademy/src/components/Articles.js
@@ -4,15 +4,15 @@ import { selectArticles, filterArticles } from "../features/articles/articlesSli
 import Search from "./Search";
 
 // Import Link and useSearchParams from React Router
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function Articles () {
   const articles = useSelector(selectArticles);
-
+  const [ searchParams, setSearchParams ] = useSearchParams();
   // Grab URLSearchParams object from useSearchParams hook
   
   // Get the queryParams from object returned from useSearchParams and set to `title`
-  const title = '';
+  const title = searchParams.get('title');
 
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
